219ee0468484af69deb9ab31e7234fd9b25e8969
Talant Mataev <talant.main@gmail.com>
2025-03-26 12:54:25 +0600

    Java Coding Assignment 25.03.2025

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,71 @@
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+interface MathOperation {
+    int operate(int a, int b);
+}
 public class Main {
     public static void main(String[] args) {
-        TaskScheduler scheduler = new TaskScheduler();
+        MathOperation addition = (a, b) -> a + b;
+        MathOperation subtraction = (a, b) -> a - b;
+        MathOperation multiplication = (a, b) -> a * b;
+        MathOperation division = (a, b) -> a / b;
 
-        scheduler.addTask(new Task("Code Review", 3, 20));
-        scheduler.addTask(new Task("System Update", 5, 45));
-        scheduler.addTask(new Task("Database Backup", 2, 30));
-        scheduler.addTask(new Task("Deploy New Feature", 5, 50));
-        scheduler.addTask(new Task("Bug Fixing", 4, 25));
+        System.out.println("Addition: " + addition.operate(5, 5));
+        System.out.println("Subtraction: " + subtraction.operate(7, 3));
+        System.out.println("Multiplication: " + multiplication.operate(7, 3));
+        System.out.println("Division: " + division.operate(6, 2));
 
-        scheduler.displayScheduledTasks();
+        List<Integer> numbers = Arrays.asList(2,3,4,5,1,-110, -11, 15, 22, 33, 40, 55);
+
+        Predicate<Integer> isOdd = number -> number % 2 != 0;
+
+        TreeSet<Integer> oddNumbers = new TreeSet<>();
+
+        for (int number : numbers) {
+            if (isOdd.test(number)) {
+                oddNumbers.add(number);
+            }
+        }
+
+        System.out.println("Odd Numbers: " + oddNumbers);
+
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "David", "Eve", "Frank", "George", "Hannah", "Ivan", "John", "Kevin", "Linda");
+        names.sort((name1, name2) -> name2.compareTo(name1));
+
+        System.out.println("Names in reverse order: " + names);
+
+
+        List<String> words = Arrays.asList("hello", "java", "lambda");
+        Function<String, String> transformString = str -> new StringBuilder(str.toUpperCase()).reverse().toString();
+
+        System.out.print("Transformed words: ");
+        words.forEach(word -> System.out.print(transformString.apply(word) + " "));
+
         System.out.println();
 
-        scheduler.processTask();
-        scheduler.delayTask();
-        scheduler.displayScheduledTasks();
-        scheduler.displayPendingTasks();
-        System.out.println();
 
-        scheduler.delayTask();
-        scheduler.displayScheduledTasks();
-        scheduler.displayPendingTasks();
-        System.out.println();
+        List<String> cities = Arrays.asList("New York", "London", "Tokyo", "Berlin");
+        Consumer<String> printCity = city -> System.out.println(city);
+        Consumer<String> printCityRef = System.out::println;
 
-        scheduler.processTask();
-        scheduler.displayScheduledTasks();
-        scheduler.displayPendingTasks();
-        System.out.println();
 
-        scheduler.processTask();
-        scheduler.displayScheduledTasks();
-        scheduler.displayPendingTasks();
-        System.out.println();
+        cities.forEach(printCity);
+        cities.forEach(printCityRef);
 
-        scheduler.processPendingTask();
-        scheduler.displayScheduledTasks();
-        scheduler.displayPendingTasks();
-        System.out.println();
+        BiFunction<Integer, Integer, Integer> max = (a, b) -> {
+            if (a > b) return a; else return b;};
+        System.out.println("Max: " + max.apply(40,2));
+        BiFunction<Integer, Integer, Integer> min = (a, b) -> {
+            if (a < b) return a; else return b;};
+        System.out.println("Min: " + min.apply(40,2));
 
-        scheduler.processPendingTask();
-        scheduler.displayScheduledTasks();
-        scheduler.displayPendingTasks();
     }
 }
+
+
+
+
